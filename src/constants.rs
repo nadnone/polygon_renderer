045a77f5d67e1571b2df996d6b0ed0a890568676
Nad <34311347+nadnone@users.noreply.github.com>
2022-08-23 19:33:25 +0200

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
+pub const PI: f32 = 3.141592653589793;
+
 pub const WIDTH:f32 = 800.0;
 pub const HEIGHT:f32 = 640.0;
 pub const HALF_HEIGHT: i32 = HEIGHT as i32 / 2;
@@ -9,7 +11,6 @@ pub const HEIGHT_LOGIC: i32 = HALF_HEIGHT;
 
 pub const FPS: f32 = 1.0/60.0;
 
-pub const FOV: f32 = 68.0 * 3.1415 / 180.0;
+pub const FOV: f32 = 68.0 * PI / 180.0;
 pub const LAMBDA: f32 = 100.0 / (100. - 1.); // F_FAR / (Z_FAR - Z_NEAR)
 
-pub const PI: f32 = 3.141592653589793;
